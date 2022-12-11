8950b8d0d5966f516d196b0c0b2091df6ce2661b
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-12-11 23:13:49 +0300

    Users are added- everything is fine

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,2 +1,20 @@
 class MapsController < ApplicationController
+    before_action :authenticate_user!
+
+    def index
+
+    end
+
+    def result
+        @latitude = params[:latitude]
+        @longitude = params[:longitude]
+        @user = current_user
+        @user.latitude = @latitude
+        @user.longitude = @longitude
+        @user.save
+    end
+
+    
+
+private
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "maps#index"
+  post '/result', to: 'maps#result'
+  get 'users/index', as: :user
+  get 'users/show', as: :user_show
+  get 'users/sign_out', as: :user_sign_out
+  get 'users/sign_in', as: :user_sign_in
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index]
 end
