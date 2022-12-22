1ab7ccf1be61c271b371baff3181e411912e1937
Ivan Pogiba <pogibuskaivan@gmail.com>
2022-12-22 03:32:32 +0300

    All tests are added and worked nice

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -18,12 +18,12 @@ RSpec.describe Devise, type: :system do
       fill_in 'Password', with: password_user
       fill_in 'Password confirmation', with: password_user
       click_button 'Sign up'
-      expect(page).to have_content("error prohibited this user from being saved:\nEmail can't be blank\n")
+      expect(page).to have_content("4 errors prohibited this user from being saved:\nEmail can't be blank\n")
     end
 
     scenario 'sign up without password' do
       visit new_user_registration_path
-      fill_in 'Email', with: email_user
+      fill_in 'email-input', with: email_user, wait: 10
       fill_in 'Password confirmation', with: password_user
       click_button 'Sign up'
       expect(page).to have_content("Password confirmation doesn't match Password\n")
@@ -31,7 +31,7 @@ RSpec.describe Devise, type: :system do
 
     scenario 'sign up, but password confirmation does not match password' do
       visit new_user_registration_path
-      fill_in 'Email', with: email_user
+      fill_in 'email-input', with: email_user, wait: 10
       fill_in 'Password', with: password_user
       fill_in 'Password confirmation', with: password_user + '1'
       click_button 'Sign up'
@@ -40,8 +40,8 @@ RSpec.describe Devise, type: :system do
     
     scenario 'sign up with correct params' do
       visit new_user_registration_path
-      fill_in 'Email', with: email_user
-      fill_in 'Password', with: password_user
+      fill_in 'email-input', with: email_user, wait: 10
+      fill_in 'password-input', with: password_user, wait: 10
       fill_in 'Password confirmation', with: password_user
       fill_in 'Full name', with: full_name
       click_button 'Sign up'
@@ -52,12 +52,12 @@ RSpec.describe Devise, type: :system do
   describe 'User Sign In' do
     before :each do
       visit new_user_registration_path
-      fill_in 'Email', with: email_user
-      fill_in 'Password', with: password_user
+      fill_in 'email-input', with: email_user, wait: 10
+      fill_in 'password-input', with: password_user, wait: 10
       fill_in 'Password confirmation', with: password_user
       fill_in 'Full name', with: full_name
       click_button 'Sign up'
-      click_link 'Log out'
+      find('#log_out').click
     end
   
     scenario 'sign in without params' do
@@ -66,8 +66,8 @@ RSpec.describe Devise, type: :system do
     end
 
     scenario 'sign in with params' do
-      fill_in 'Email', with: email_user
-      fill_in 'Password', with: password_user
+      fill_in 'email-input', with: email_user, wait: 10
+      fill_in 'password-input', with: password_user, wait: 10
       click_button 'Sign In'
       expect(page).to have_content("Signed in successfully.\n") 
     end
@@ -76,23 +76,19 @@ RSpec.describe Devise, type: :system do
   describe 'User page navigation' do
     before :each do
       visit new_user_registration_path
-      fill_in 'Email', with: email_user
-      fill_in 'Password', with: password_user
+      fill_in 'email-input', with: email_user, wait: 10
+      fill_in 'password-input', with: password_user, wait: 10
       fill_in 'Password confirmation', with: password_user
       fill_in 'Full name', with: full_name
       click_button 'Sign up'
     end
 
     scenario 'Navigation panel' do
-      click_link 'Show Profile'
+      find('#show_profile').click
       expect(current_path) == user_profile_path
-      click_link 'Chat'
+      find('#chat').click
       expect(current_path) == '/rooms'
-      click_link 'English'
-      expect(current_path) == '/?locale=en'
-      click_link 'Русский'
-      expect(current_path) == '/?locale=ru'
-      click_link 'Maply'
+      find('#root').click
       expect(current_path) == root_path
     end
   end
@@ -100,21 +96,14 @@ RSpec.describe Devise, type: :system do
   describe 'Interaction between of two users' do
     let(:user_one) {User.create(email: "kochka_pochka@mail.ru", password: "rijqjr;3rewmnrwea", full_name: "Zatochka Pochka" )}
     let(:user_two) {User.create(email: "ivan_sobachka_tachka@gmail.ru", password: "Nn;kgwJKBmnvsdBHJfs", full_name: "Ivan Smallgiba" )}
+    let(:message) {"Hello!"}
 
     def user_sign_in(user)
+      sleep(5)
       visit new_user_session_path
-      fill_in 'Email', with: user.email
-      fill_in 'Password', with: user.password
+      fill_in 'email-input', with: user.email, wait: 10
+      fill_in 'password-input', with: user.password, wait: 10
       click_button 'Sign In'
-    end
-
-    def user_one_request_user_two_accpet(user)
-      fill_in 'Search user', with: user.full_name
-      click_on 'Search'
-      click_on 'Add'
-      click_link 'Log out'
-      user_sign_in(user_one)
-      click_on 'Accept'
     end
 
     def reload_page
@@ -129,9 +118,13 @@ RSpec.describe Devise, type: :system do
 
     scenario 'Visit Show Profile' do
       visit user_profile_path
+      expect(page).to have_content(user_two.full_name)
+      expect(page).to have_content(user_two.email)
+      expect(page).to have_content("You haven't got any friends request")
     end
 
     scenario 'Click on find nearest person' do
+      sleep(5)
       click_button 'Find nearest person to you'
       expect(page).to have_content(user_one.email)
       expect(page).to have_button('Add')
@@ -159,7 +152,7 @@ RSpec.describe Devise, type: :system do
       fill_in 'Search user', with: user_one.full_name
       click_on 'Search'
       click_on 'Add'
-      click_link 'Log out'
+      find('#log_out').click
       expect(current_path) == :user_sign_in
       user_sign_in(user_one)
       expect(page).to have_content(user_one.full_name)
@@ -167,30 +160,100 @@ RSpec.describe Devise, type: :system do
     end
 
     scenario 'User_one accept user_two in Show profile' do
-      fill_in 'Search user', with: user_one.full_name
+      fill_in 'Search user', with: user_one.full_name, wait: 10
       click_on 'Search'
       click_on 'Add'
-      click_link 'Log out'
+      sleep(5)
+      find('#log_out').click
       expect(current_path) == :user_sign_in
       user_sign_in(user_one)
-      visit user_profile_path
+      sleep(5)
+      find('#show_profile').click
       expect(page).to have_content(user_one.full_name)
-      click_on 'Accept'
+      click_button 'Accept'
+      expect(page).to have_content(user_one.full_name)
     end
 
     scenario 'User_one find nearest friend' do
-      user_one_request_user_two_accpet(user_one)
+      fill_in 'Search user', with: user_one.full_name
+      click_on 'Search'
+      click_on 'Add'
+      find('#log_out').click
+      expect(current_path) == :user_sign_in
+      user_sign_in(user_one)
+      expect(page).to have_content(user_one.full_name)
+      click_on 'Accept'
       reload_page
+      sleep(5)
       click_button 'Find nearest friend to you'
       expect(page).to have_content(user_one.full_name)
       expect(page).to have_content(user_one.email)
-      expect(page).to have_button('Chat')
+      expect(page).to have_link('Chat')
+      find('#chat_btn').click
+      expect(current_path) == '/rooms'
+      sleep(5)
+      find('#log_out').click
     end
 
-    scenario 'Chat with another user' do
-      user_one_request_user_two_accpet(user_one)
-      click_on user_one.full_name
-      click_on 'Chat'
+    scenario 'User_one send message to user_two' do
+      fill_in 'Search user', with: user_one.full_name
+      click_on 'Search'
+      click_on 'Add'
+      find('#log_out').click
+      expect(current_path) == :user_sign_in
+      user_sign_in(user_one)
+      expect(page).to have_content(user_one.full_name)
+      click_on 'Accept'
+      reload_page
+      sleep(5)
+      find('#chat').click
+      find("#user_#{user_two.id}").click
+      fill_in 'chat-text', with: message
+      click_on 'Send'
+      expect(page).to have_content('less than a minute ago')
+      expect(page).to have_content(message)
+      sleep(5)
+      find('#log_out').click
+      user_sign_in(user_two)
+      sleep(5)
+      find('#chat').click
+      find("#user_#{user_one.id}").click
+      expect(page).to have_content(message)
+    end
+
+    scenario 'User_one add friend from user_two profile' do
+      fill_in 'Search user', with: user_one.full_name
+      click_on 'Search'
+      click_on 'Add'
+      find('#log_out').click
+      expect(current_path) == :user_sign_in
+      user_sign_in(user_one)
+      expect(page).to have_content(user_one.full_name)
+      click_on 'Accept'
+      reload_page
+      sleep(5)
+      user_three = User.create(email: "boba_biba@gmail.ru", password: "Nn;kgwJKBmnvsdBHJfs", full_name: "Egor Biggiba" )
+      sleep(5)
+      find('#log_out').click
+      user_sign_in(user_three)
+      fill_in 'Search user', with: user_two.full_name
+      click_on 'Search'
+      click_on 'Add'
+      sleep(5)
+      find('#log_out').click
+      user_sign_in(user_two)
+      click_on 'Accept'
+      sleep(10)
+      find('#log_out').click
+      user_sign_in(user_three)
+      sleep(5)
+      find('#show_profile').click
+      expect(page).to have_content(user_two.full_name)
+      find("#user_#{user_two.id}").click
+      expect(page).to have_content(user_one.full_name)
+      find("#user_#{user_one.id}").click
+      expect(page).to have_button('Add')
+      sleep(20)
     end
   end
 end
